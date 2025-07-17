11aa3de5a15949846d0eb0f856eed2020f0e6d27
digiflexlab <digiflexlab.ai@gmail.com>
2025-07-17 16:14:03 +0100

    Update SearchAndFilter.tsx

--- src/components/common/SearchAndFilter.tsx
+++ src/components/common/SearchAndFilter.tsx
@@ -24,6 +24,8 @@ export const SearchAndFilter = ({
           <div className="flex-1 relative">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
             <Input
+              id="product-search-input"
+              name="product-search"
               placeholder={searchPlaceholder}
               value={searchValue}
               onChange={(e) => onSearchChange(e.target.value)}
